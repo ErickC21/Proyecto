00a945662539fd15f9aa0da8f5dd78815e58c1f7
ErickC21 <112288713+ErickC21@users.noreply.github.com>
2024-06-01 22:26:10 -0600

    Create README.java

---
+++ README.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Proyecto;
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Menu movie = new Menu();
+        movie.menuPrincipal();
+    }
+}
